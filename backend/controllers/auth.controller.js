fc1ac1828bd857bc5b961b4b516a6ee839e01665
Satish Chaurasiya <165125161+satish4397@users.noreply.github.com>
2025-11-02 18:58:41 +0530

    updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const sighUp=async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "strict",
+            sameSite: "",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
