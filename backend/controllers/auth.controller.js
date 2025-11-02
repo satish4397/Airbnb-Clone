7ea63cd39ae9b38b749c4f70eebba3d34c8a31b6
Satish Chaurasiya <165125161+satish4397@users.noreply.github.com>
2025-11-02 19:01:39 +0530

    updated

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -15,7 +15,7 @@ export const sighUp=async (req,res) => {
         res.cookie("token",token,{
             httpOnly:true,
             secure:true,
-            sameSite: "",
+            sameSite: " ",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
@@ -41,8 +41,8 @@ export const login = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENVIRONMENT = "production",
-            sameSite: "strict",
+            secure:true,
+            sameSite: " ",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
 
